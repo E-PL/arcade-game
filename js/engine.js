1640dea8d8310fbae37938e7ada033a0e9635619
e-pl <507904+E-PL@users.noreply.github.com>
2018-05-07 21:14:15 +0200

    fix: fix heart image not loading

--- js/engine.js
+++ js/engine.js
@@ -191,7 +191,7 @@ var Engine = (function(global) {
         'images/char-horn-girl.png',
         'images/char-princess-girl.png',
         'images/char-pink-girl.png',
-        'images/heart.png',
+        'images/Heart.png',
         'images/Gem Green.png',
         'images/Gem Blue.png',
         'images/Gem Orange.png',
